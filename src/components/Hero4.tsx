64f93e1be9e277e72de7cbbfa6b6a0674a8ad54d
DominicDev <s0l0estudi4r@gmail.com>
2025-06-21 18:31:01 +0200

    v0.1.0

--- src/components/Hero4.tsx
+++ src/components/Hero4.tsx
@@ -1,8 +1,32 @@
-import '@/styles/Hero4.css'; 
+import '@/styles/Hero4.css';
 import { LiteYoutube } from '@/components/LiteYoutube';
+import { InstagramLogo } from '@/components/InstagramLogo';
+import { Xlogo } from '@/components/Xlogo';
+import { TwitchLogo } from '@/components/TwitchLogo';
+import { YoutubeLogo } from '@/components/YoutubeLogo';
+
+const arrayLogos = [
+  {
+    Element: InstagramLogo
+  },
+  {
+    Element: Xlogo
+  },
+  {
+    Element: TwitchLogo
+  },
+  {
+    Element: YoutubeLogo
+  }
+];
 
 export function Hero4() {
-  return <article className='hero4 container-hero'>
-    <LiteYoutube />
-  </article>;
+  return (
+    <article className='hero4 container-hero'>
+      <LiteYoutube />
+      {arrayLogos.map(({ Element }) => (
+        <Element className='logo-social-network' />
+      ))}
+    </article>
+  );
 }
