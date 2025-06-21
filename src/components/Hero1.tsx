b230da8395c800b7bb81b4a2374aa2f9e3676c85
DominicDev <s0l0estudi4r@gmail.com>
2025-06-21 13:34:13 +0200

    v0.0.2

--- src/components/Hero1.tsx
+++ src/components/Hero1.tsx
@@ -4,14 +4,17 @@ import { ButtonDownload } from '@/components/ButtonDownload';
 import { LuxuryButton } from './LuxuryButton';
 const carGrefgImage = withBase('/assets/car-grefg-3.webp');
 const lightRigth = withBase('/assets/lightb-right-4.webp');
+const lightLeft = withBase('/assets/light-left-5.webp');
+const theGrefImage = withBase('/assets/thegrefg-1.webp');
 
 export function Hero1() {
   return (
     <article className='hero1 container-hero'>
       <aside className='container-inner container-inner-left'>
+        <img className='light-image-left' src={lightLeft} alt='Light Left Image' />
         <section className='container-content'>
           <LuxuryButton />
-          <h3>¡El coche de TheGrefg puede ser tuyo!</h3>
+          <h3>¡El coche de TheGrefg <img className='the-grefg' src={theGrefImage} alt="The Grefg Logo" /> puede ser tuyo!</h3>
           <p>
             Compra 2 wallpapers exclusivos y consigue tu participación para el
             sorteo del coche de TheGrefg.
