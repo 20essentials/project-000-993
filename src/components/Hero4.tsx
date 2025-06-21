a6bfc5778c6c92b49652578e6a61ad1a5377fb3d
DominicDev <s0l0estudi4r@gmail.com>
2025-06-21 19:01:04 +0200

    v0.1.1

--- src/components/Hero4.tsx
+++ src/components/Hero4.tsx
@@ -4,19 +4,29 @@ import { InstagramLogo } from '@/components/InstagramLogo';
 import { Xlogo } from '@/components/Xlogo';
 import { TwitchLogo } from '@/components/TwitchLogo';
 import { YoutubeLogo } from '@/components/YoutubeLogo';
+import { withBase } from '@/utils/functionsTs';
+const thunderImg = withBase('/assets/thunder.gif');
 
 const arrayLogos = [
   {
-    Element: InstagramLogo
+    Element: InstagramLogo,
+    nombreDeClase: 'logo-ig',
+    red: 'https://www.instagram.com/thegrefg'
   },
   {
-    Element: Xlogo
+    Element: Xlogo,
+    nombreDeClase: 'logo-x',
+    red: 'https://x.com/TheGrefg'
   },
   {
-    Element: TwitchLogo
+    Element: TwitchLogo,
+    nombreDeClase: 'logo-twitch',
+    red: 'https://www.twitch.tv/thegrefg'
   },
   {
-    Element: YoutubeLogo
+    Element: YoutubeLogo,
+    nombreDeClase: 'logo-youtube',
+    red: 'https://www.youtube.com/channel/UCCEmjNPpJYhGDgaEqeeA4HA'
   }
 ];
 
@@ -24,8 +34,11 @@ export function Hero4() {
   return (
     <article className='hero4 container-hero'>
       <LiteYoutube />
-      {arrayLogos.map(({ Element }) => (
-        <Element className='logo-social-network' />
+      {arrayLogos.map(({ Element, nombreDeClase, red }) => (
+        <a target='_blank' href={red}>
+          <Element className={`logo-social-network ${nombreDeClase}`} />
+          <img src={thunderImg} alt='Thunder Gif' className={`thunder thunder-${nombreDeClase}`} />
+        </a>
       ))}
     </article>
   );
